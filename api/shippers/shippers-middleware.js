62f9dec9a7695b0adef3b8f5c201cdc5293c46f3
Sydney <proteimus@gmail.com>
2025-06-13 15:18:38 -0500

    update shippers-model n shippers-middleware

--- api/shippers/shippers-middleware.js
+++ api/shippers/shippers-middleware.js
@@ -1,12 +1,35 @@
+const db = require('../../data/db-config.js')
+
 module.exports = {
   checkId,
   checkPayload,
 }
 
 function checkId(req, res, next) {
-  next()
+  const { id } = req.params
+  db('shippers')
+    .where({ shipperId: id })
+    .first()
+    .then(shipper => {
+      if (!shipper) {
+        next({ status: 404, message: 'Shipper not found' })
+      } else {
+        next()
+      }
+    })
+    .catch(err => {
+      next({ status: 500, message: 'Error checking shipper ID', error: err })
+    })
+
 }
 
 function checkPayload(req, res, next) {
-  next()
+
+  const { phone, shipperName } = req.body
+  if (!shipperName || !phone) {
+    next({ status: 422, message: 'shipperName and phone are required' })
+  }
+  else {
+    next()
+  }
 }
--- api/shippers/shippers-model.js
+++ api/shippers/shippers-model.js
@@ -1,3 +1,5 @@
+const db = require('../../data/db-config')
+
 module.exports = {
   get,
   getById,
@@ -7,21 +9,37 @@ module.exports = {
 }
 
 async function get() {
-  return 'get wired'
+  const shippers = await db('shippers')
+  return shippers.map(shipper => ({
+    ...shipper,
+    phone: shipper.phone ? shipper.phone : null,
+  }))
 }
 
-async function getById() {
-  return 'getById wired'
+async function getById(shipperId) {
+  const shipper = await db('shippers').where('shipperId', shipperId).first()
+  if (!shipper) {
+    return null
+  }
+  return {
+    ...shipper,
+    phone: shipper.phone ? shipper.phone : null,
+  }
 }
 
-async function create() {
-  return 'create wired'
+async function create(newShipper) {
+  const [shipperId] = await db('shippers').insert(newShipper)
+  return getById(shipperId)
 }
 
-async function update() {
-  return 'update wired'
+async function update(shipperId, changes) {
+  const shipper = await getById(shipperId)
+  await db('shippers').where('shipperId', shipperId).update(changes)
+  return getById(shipperId)
 }
 
-async function remove() {
-  return 'delete wired'
+async function remove(shipperId) {
+  const shipper = await getById(shipperId)
+  await db('shippers').where('shipperId', shipperId).del()
+  return shipper
 }
